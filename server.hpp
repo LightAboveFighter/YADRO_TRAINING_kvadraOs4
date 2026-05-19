eb4469021329e164a97d359bf0db776de0099be2
Alexunder <skosarigin@gmail.com>
2026-05-19 12:24:03 +0300

    fix SO_REUSEADDR socket flag usage

--- server.hpp
+++ server.hpp
@@ -14,8 +14,8 @@ class Server {
   Server(std::string address, int port,
          std::shared_ptr<ThreadResourceController>& controller)
       : main_socket{AF_INET, SOCK_STREAM, 0}, thread_controller{controller} {
-    main_socket.bind_s(AF_INET, port, string_to_in_addr_t(address));
     main_socket.set_socket_option(SO_REUSEADDR, 1);
+    main_socket.bind_s(AF_INET, port, string_to_in_addr_t(address));
   }
 
   ~Server() { main_socket.close_s(); }
